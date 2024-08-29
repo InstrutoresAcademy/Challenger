af8f4f5107bf0f1ad7e46dcd93d4c3b00764b568
gabriel diniz <gabrieldiniz107@gmail.com>
2024-08-29 18:48:11 -0300

    removendo todos os arquivos

--- ex03.py
+++
@@ -1,9 +0,0 @@
-# Crie um programa que permite ao usuário inserir três itens de uma lista de compras. Em seguida, exiba a lista completa.
-
-minha_lista =[]
-
-for i in range(3):
-    item = input(f'Informe o {i + 1}° item da lista: ')
-    minha_lista.append(item)
-
-print('Lista de compras: ', minha_lista)
--- exemplo.py
+++
@@ -1,5 +0,0 @@
-frutas = ["maçã", "banana", "laranja", "uva", "manga"]
-
-#percorrer a lista com for
-for fruta in frutas:
-    print(fruta)
